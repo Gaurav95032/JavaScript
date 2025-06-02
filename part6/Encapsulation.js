5900a6bbf89b124bc7cf33bd5a741063549a2bec
[Gaurav Waghmare] <gauravwaghmare95032@gmail.com>
2025-06-02 21:55:06 +0530

    Encapsulation

---
+++ part6/Encapsulation.js
@@ -0,0 +1,17 @@
+class BankAccount{
+    #Balance = 0;
+
+    Deposit(amount) {
+        this.#Balance += amount;
+        return this.#Balance;
+    }
+
+    CheckBalance() {
+        return `your bank balance is ${this.#Balance} $.`;
+    }
+
+}
+
+let myAccount = new BankAccount();
+myAccount.Deposit(10000);
+console.log(myAccount.CheckBalance());

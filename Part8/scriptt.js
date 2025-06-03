711ba673005ddb5ac5f20badca9d7e6d4ae0be1e
[Gaurav Waghmare] <gauravwaghmare95032@gmail.com>
2025-06-03 15:48:15 +0530

    Dom2

--- Part8/scriptt.js
+++ Part8/scriptt.js
@@ -4,3 +4,23 @@ document.getElementById('paragraphChangeButton').addEventListener('click', funct
     let change = document.getElementById('myParagraph');
     change.textContent = "This paragraph is changed."
 })
+
+
+// example 2
+
+document.getElementById('highlightFirstCity').addEventListener('click', function () {
+    let cities = document.getElementById('citiesList');
+    let firstCity = cities.firstElementChild;
+
+    if (firstCity) {
+        firstCity.classList.add('bg-yellow-300', 'text-black', 'px-3', 'py-2');
+    }
+});
+
+
+// example 3
+
+document.getElementById('changeOrder').addEventListener('click', function () {
+    let coffeeOrder = document.getElementById('coffeeType');
+    coffeeOrder.textContent = "Expresso";
+})
